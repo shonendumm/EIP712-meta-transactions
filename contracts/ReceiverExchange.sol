c0353e3bc047160c2879f8e6cfaaef3214f7fab7
Soo Hian <shonendumm@gmail.com>
2022-02-01 18:02:04 +0800

    add compound example code

--- contracts/ReceiverExchange.sol
+++ contracts/ReceiverExchange.sol
@@ -82,3 +82,25 @@ contract ReceiverExchange {
 }
 
 
+/**
+
+function delegateBySig(address delegatee, uint nonce, uint expiry, uint8 v, bytes32 r, bytes32 s) public {
+        bytes32 domainSeparator = keccak256(abi.encode(DOMAIN_TYPEHASH, keccak256(bytes(name)), getChainId(), address(this)));
+        bytes32 structHash = keccak256(abi.encode(DELEGATION_TYPEHASH, delegatee, nonce, expiry));
+        bytes32 digest = keccak256(abi.encodePacked("\x19\x01", domainSeparator, structHash));
+        address signatory = ecrecover(digest, v, r, s);
+        require(signatory != address(0), "Comp::delegateBySig: invalid signature");
+        require(nonce == nonces[signatory]++, "Comp::delegateBySig: invalid nonce");
+        require(now <= expiry, "Comp::delegateBySig: signature expired");
+        return _delegate(signatory, delegatee);
+    }
+
+
+    function getChainId() internal pure returns (uint) {
+        uint256 chainId;
+        assembly { chainId := chainid() }
+        return chainId;
+    }
+
+ */
+
