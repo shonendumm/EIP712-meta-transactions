4be3fe76eafb40a768426df212fba243c6d76c29
Soo Hian <shonendumm@gmail.com>
2022-02-02 16:24:10 +0800

    added example contact code

--- contracts/ReceiverExchange.sol
+++ contracts/ReceiverExchange.sol
@@ -31,6 +31,12 @@ contract ReceiverExchange {
 
     }
 
+        /// @notice The EIP-712 typehash for the contract's domain
+    bytes32 public constant DOMAIN_TYPEHASH = keccak256("EIP712Domain(string name,uint256 chainId,address verifyingContract)");
+
+    /// @notice The EIP-712 typehash for the delegation struct used by the contract
+    bytes32 public constant DELEGATION_TYPEHASH = keccak256("Delegation(address delegatee,uint256 nonce,uint256 expiry)");
+
 
     constructor(address _esterToken) {
         esterToken = _esterToken;
