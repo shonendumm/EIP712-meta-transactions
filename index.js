542036855db0f4d07783fde5d5814a5bb6131aa3
Soo Hian <shonendumm@gmail.com>
2022-02-13 12:33:26 +0800

    fixed send by node server

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ require('dotenv').config()
 const Web3 = require('web3');
 
 // to enter an infura endpoint URL
-const web3 = new Web3(Web3.givenProvider || "ws://localhost:8545");
+const web3 = new Web3(Web3.givenProvider || process.env.RINKEBY_RPC_URL);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`listening at port ${port}`));
@@ -32,14 +32,16 @@ app.post('/send-buy-order', (request, response) => {
 })
 
 
-const private_key = process.env.SERVER_PRIVATE_KEY;
+const private_key = process.env.PRIVATE_KEY;
 const serverWallet = web3.eth.accounts.wallet.add(private_key);
 
 const exchangeAbiFile = require('./public/exchange-abi.js');
 const exchangeAbi = exchangeAbiFile.exchangeAbi;
+
 const exchangeAddress = '0xadd39d12aD9b8FFe3DCB48ac3822b94DD1308d2E';
 const exchangeContract = new web3.eth.Contract(exchangeAbi, exchangeAddress);
-
+// exchangeContract.defaultChain = 'rinkeby';
+// console.log("contract chain is at", exchangeContract.defaultChain);
 
 
 // 30 seconds
@@ -49,11 +51,10 @@ setInterval(async() => {
         console.log("Auto Batch Send called by timer")
         if (BUY_ORDERS_ARRAY.length > 0) {
             console.log("Auto Batch Send called: sent transactions")
-
             const tx = await exchangeContract.methods.handleBatchOrders(BUY_ORDERS_ARRAY).send({
                 from: serverWallet.address,
-                gasLimit: web3.utils.toHex(30000000), // set gas limit and gas price here
-                gasPrice: web3.utils.toHex(20000000000), // 29970648 
+                gasLimit: web3.utils.toHex(20000000), // set gas limit and gas price here
+                gasPrice: web3.utils.toHex(2000000000), // 29970648 1480000000
             });
             // reset the orders array
             BUY_ORDERS_ARRAY = [];
