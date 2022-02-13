29538d8168dd0ee7c4b6473db5cae4710710f72a
Soo Hian <shonendumm@gmail.com>
2022-02-13 10:18:15 +0800

    refactor to node server

--- index.js
+++ index.js
@@ -1,43 +1,43 @@
 const express = require('express');
 const app = express();
+const Datastore = require('nedb');
 require('dotenv').config()
 
+// use web3
 const Web3 = require('web3');
 const web3 = new Web3(Web3.givenProvider || "ws://localhost:8545");
 
 
 const port = process.env.PORT || 3000;
-
 app.listen(port, () => console.log(`listening at port ${port}`));
-
 app.use(express.static('public'));
 app.use(express.json({ limit: '1mb' }));
 
 
-
 let BUY_ORDERS_ARRAY = [];
+const database = new Datastore('database.db');
+database.loadDatabase();
 
 app.post('/send-buy-order', (request, response) => {
     const data = request.body;
+
     console.log("From user", data);
     response.json({ message: "received order!" });
 
-    BUY_ORDERS_ARRAY.push({
-        account,
-        amount,
-        bidPrice,
-        v,
-        r,
-        s
-    });
+    database.insert(data);
+    BUY_ORDERS_ARRAY.push(data);
 
 
 })
 
 
-
 const private_key = process.env.SERVER_PRIVATE_KEY;
 const serverWallet = web3.eth.accounts.wallet.add(private_key);
+
+const exchangeAbiFile = require('./public/exchange-abi.js');
+const exchangeAbi = exchangeAbiFile.exchangeAbi;
+const exchangeAddress = '0xadd39d12aD9b8FFe3DCB48ac3822b94DD1308d2E';
+const exchangeContract = new web3.eth.Contract(exchangeAbi, exchangeAddress);
 
 
 
---
+++ public/exchange-abi.js
@@ -0,0 +1,161 @@
+module.exports = {
+    exchangeAbi: [{
+            "inputs": [{
+                "internalType": "address",
+                "name": "_esterToken",
+                "type": "address"
+            }],
+            "stateMutability": "nonpayable",
+            "type": "constructor"
+        },
+        {
+            "anonymous": false,
+            "inputs": [{
+                    "indexed": true,
+                    "internalType": "address",
+                    "name": "sender",
+                    "type": "address"
+                },
+                {
+                    "indexed": false,
+                    "internalType": "uint256",
+                    "name": "amount",
+                    "type": "uint256"
+                }
+            ],
+            "name": "BoughtESTR",
+            "type": "event"
+        },
+        {
+            "inputs": [],
+            "name": "DOMAIN_TYPEHASH",
+            "outputs": [{
+                "internalType": "bytes32",
+                "name": "",
+                "type": "bytes32"
+            }],
+            "stateMutability": "view",
+            "type": "function"
+        },
+        {
+            "inputs": [],
+            "name": "EstrContract",
+            "outputs": [{
+                "internalType": "contract IEsterToken",
+                "name": "",
+                "type": "address"
+            }],
+            "stateMutability": "view",
+            "type": "function"
+        },
+        {
+            "inputs": [],
+            "name": "ORDER_TYPEHASH",
+            "outputs": [{
+                "internalType": "bytes32",
+                "name": "",
+                "type": "bytes32"
+            }],
+            "stateMutability": "view",
+            "type": "function"
+        },
+        {
+            "inputs": [{
+                "components": [{
+                        "internalType": "address",
+                        "name": "account",
+                        "type": "address"
+                    },
+                    {
+                        "internalType": "uint256",
+                        "name": "amount",
+                        "type": "uint256"
+                    },
+                    {
+                        "internalType": "uint256",
+                        "name": "bidPrice",
+                        "type": "uint256"
+                    },
+                    {
+                        "internalType": "uint8",
+                        "name": "v",
+                        "type": "uint8"
+                    },
+                    {
+                        "internalType": "bytes32",
+                        "name": "r",
+                        "type": "bytes32"
+                    },
+                    {
+                        "internalType": "bytes32",
+                        "name": "s",
+                        "type": "bytes32"
+                    }
+                ],
+                "internalType": "struct TokenExchange.BuyOrder[]",
+                "name": "_batchOrders",
+                "type": "tuple[]"
+            }],
+            "name": "handleBatchOrders",
+            "outputs": [],
+            "stateMutability": "nonpayable",
+            "type": "function"
+        },
+        {
+            "inputs": [{
+                    "internalType": "address",
+                    "name": "_account",
+                    "type": "address"
+                },
+                {
+                    "internalType": "uint256",
+                    "name": "_amount",
+                    "type": "uint256"
+                },
+                {
+                    "internalType": "uint256",
+                    "name": "_bidPrice",
+                    "type": "uint256"
+                },
+                {
+                    "internalType": "uint8",
+                    "name": "v",
+                    "type": "uint8"
+                },
+                {
+                    "internalType": "bytes32",
+                    "name": "r",
+                    "type": "bytes32"
+                },
+                {
+                    "internalType": "bytes32",
+                    "name": "s",
+                    "type": "bytes32"
+                }
+            ],
+            "name": "handleEachOrder",
+            "outputs": [{
+                "internalType": "bool",
+                "name": "",
+                "type": "bool"
+            }],
+            "stateMutability": "nonpayable",
+            "type": "function"
+        },
+        {
+            "inputs": [],
+            "name": "name",
+            "outputs": [{
+                "internalType": "string",
+                "name": "",
+                "type": "string"
+            }],
+            "stateMutability": "view",
+            "type": "function"
+        },
+        {
+            "stateMutability": "payable",
+            "type": "receive"
+        }
+    ]
+}
